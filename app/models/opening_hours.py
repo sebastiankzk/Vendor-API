6848e5fbe35e3894f32eb98d4f15452cbf063dd5
sebastiankzk <40329724+sebastiankzk@users.noreply.github.com>
2024-10-26 16:07:37 +0800

    renaming openinghours to opening_hours

--- app/models/opening_hours.py
+++ app/models/opening_hours.py
@@ -3,7 +3,7 @@ from app.models import Base
 from app.models.vendor_profile import VendorProfile
 
 class OpeningHours(Base):
-    __tablename__ = "openinghours"
+    __tablename__ = "opening_hours"
 
     openingHoursID = Column(Integer, primary_key=True)
     vendorProfileID = Column(Integer, ForeignKey(VendorProfile.vendorProfileID))
